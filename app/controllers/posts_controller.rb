653020cb8ea803ae9ee81e51bc64e77ec2b61ee3
Walter <wxfwalter@gmail.com>
2017-08-30 07:46:10 +0800

    finish lesson 1 - 3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,51 @@
-class PostsController < ApplicationController
-  def index
+require 'pry'
 
+
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = '1'
+
+    if @post.save
+      flash["notice"] = 'New post is created!'
+      redirect_to posts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:notice] = 'The post is updated.'
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :url, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
---
+++ app/models/post.rb
@@ -0,0 +1,10 @@
+class Post < ActiveRecord::Base
+  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  has_many :comments
+  has_many :post_categories
+  has_many :categories, through: :post_categories
+
+  validates :title, :url, :description, presence: true
+  validates :title, length: {minimum: 5}
+  validates :url, uniqueness: true
+end
