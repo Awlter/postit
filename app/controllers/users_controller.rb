d1b73516b98f7de7e9f7656f1a4fa8003c1daf2e
Walter <wxfwalter@gmail.com>
2017-09-03 16:14:59 +0800

    new user create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
+require 'pry'
+
 class UsersController < ApplicationController
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(params.require(:user).permit!)
+
+    if @user.save
+      flash["notice"] = "New user has been created successfully."
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 end
