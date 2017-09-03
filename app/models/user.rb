4d982caf05d85e4b42c8e74f7bf6a1af337ac7ba
Walter <wxfwalter@gmail.com>
2017-09-03 16:07:06 +0800

    add password digest to users, new routes, controller, validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
+
+  has_secure_password validation: false
+
+  validates :username, :password, :password_confirmation, presence: true
+  validates :username, length: {minimum: 6}, uniqueness: true
+  validates :password, length: {minimum: 8}
 end
